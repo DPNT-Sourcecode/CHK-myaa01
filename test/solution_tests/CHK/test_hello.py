b038fff20b774b0e80cf6e8e95a86ee3ea7b16ea
sbx_user1051 <sbx_user1051@169.254.43.53>
2020-01-28 14:57:16 +0000

    Tue Jan 28 14:57:16 UTC 2020

--- test/solution_tests/CHK/test_hello.py
+++ test/solution_tests/CHK/test_hello.py
@@ -1,7 +1,10 @@
+import pytest
+
 from solutions.CHK import checkout_solution
 
 
 class TestCheckout():
-    def test_checkout(self):
-        assert checkout_solution.checkout()
+    def test_illegal_input(self):
+        assert checkout_solution.checkout('ABCDE')
+
 
