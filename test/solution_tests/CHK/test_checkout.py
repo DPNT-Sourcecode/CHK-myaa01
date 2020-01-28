1536959aecd8ce3b834bcf389d300126b82d4983
sbx_user1051 <sbx_user1051@169.254.43.53>
2020-01-28 15:41:20 +0000

    Tue Jan 28 15:41:20 UTC 2020

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -3,8 +3,8 @@ from solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_illegal_input(self):
-        """ Reject inputs that contain any letter that is not one of A,B,C,D """
-        assert checkout_solution.checkout('ABCDE') == -1
+        """ Reject inputs that contain any letter that is not one of A,B,C,D,E """
+        assert checkout_solution.checkout('ABCDEFGZ') == -1
         assert checkout_solution.checkout('J') == -1
         assert checkout_solution.checkout('abcd') == -1
         assert checkout_solution.checkout('__*&^TBN') == -1
@@ -19,5 +19,11 @@ class TestCheckout():
     def test_multiple_priced_skus(self):
         assert checkout_solution.checkout('AAA') == 130
         assert checkout_solution.checkout('AAAA') == 180
+        assert checkout_solution.checkout('AAAAA') == 200
         assert checkout_solution.checkout('BB') == 45
         assert checkout_solution.checkout('BBB') == 75
+
+    def test_special_offers_combination(self):
+        assert checkout_solution.checkout('BEE') == 80
+        assert checkout_solution.checkout('BBEE') == 110
+
