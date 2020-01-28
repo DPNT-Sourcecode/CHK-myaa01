ec4043e56ba6847a9f9ece13966c16fa589693bc
sbx_user1051 <sbx_user1051@169.254.43.53>
2020-01-28 14:51:05 +0000

    Tue Jan 28 14:51:05 UTC 2020

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -3,6 +3,7 @@ from solutions.HLO import hello_solution
 
 class TestHello():
     def test_hello(self):
-        assert hello_solution.hello('adam') == 'Hello, World!'
-        assert hello_solution.hello('toby') == 'Hello, World!'
+        assert hello_solution.hello('adam') == 'Hello, adam!'
+        assert hello_solution.hello('toby') == 'Hello, toby!'
+
 
