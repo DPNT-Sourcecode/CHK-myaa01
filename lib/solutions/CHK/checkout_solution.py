2cd7e0158e21a67a9139c8f364b685a5071a7db9
sbx_user1051 <sbx_user1051@169.254.43.53>
2020-01-28 15:15:16 +0000

    Tue Jan 28 15:15:16 UTC 2020

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,11 +1,28 @@
 
-
+from collections import Counter
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     registered_skus = ('A', 'B', 'C', 'D')
+    sku_prices = {
+        'A': {
+            'price': 50,
+        },
+        'B': {
+            'price': 30,
+        },
+        'C': {
+            'price': 20,
+        },
+        'D': {
+            'price': 15,
+        }
+    }
     if not all(sku in registered_skus for sku in skus):
         return -1
 
     if len(skus) == 0:
         return 0
+
+    for
+
