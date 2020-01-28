51dd6c7442ae4c4466017768899557a2d7a691a0
sbx_user1051 <sbx_user1051@169.254.43.53>
2020-01-28 15:00:16 +0000

    Tue Jan 28 15:00:16 UTC 2020

--- test/solution_tests/CHK/test_hello.py
+++ test/solution_tests/CHK/test_hello.py
@@ -1,10 +1,15 @@
-import pytest
-
 from solutions.CHK import checkout_solution
 
 
 class TestCheckout():
     def test_illegal_input(self):
-        assert checkout_solution.checkout('ABCDE')
+        """ Reject inputs that contain any letter that is not one of A,B,C,D """
+        assert checkout_solution.checkout('ABCDE') == -1
+        assert checkout_solution.checkout('J') == -1
+        assert checkout_solution.checkout('abcd') == -1
+        assert checkout_solution.checkout('__*&^TBN') == -1
+        assert checkout_solution.checkout('123') == -1
+
+
 
 
