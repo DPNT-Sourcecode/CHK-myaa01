ccb4c97a44687355dac54d4e44de7009286535e4
sbx_user1051 <sbx_user1051@169.254.11.197>
2020-02-02 18:08:31 +0000

    Sun Feb 02 18:08:31 UTC 2020

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -37,7 +37,3 @@ class TestCheckout():
         assert checkout_solution.checkout('FFF') == 20
         assert checkout_solution.checkout('FFFF') == 30
         assert checkout_solution.checkout('FFFFF') == 40
-
-
-
-
